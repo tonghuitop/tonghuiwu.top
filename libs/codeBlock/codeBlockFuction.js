0bd025dedcf8f7b16b289b633387594c1c48d864
Travis CI User <travis@example.org>
2022-01-23 08:09:07 +0000

    Site updated: 2022-01-23 08:09:06

---
+++ libs/codeBlock/codeBlockFuction.js
@@ -0,0 +1,3 @@
+// build time:Sun Jan 23 2022 08:08:13 GMT+0000 (Coordinated Universal Time)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
---
+++ libs/codeBlock/codeLang.js
@@ -0,0 +1,3 @@
+// build time:Sun Jan 23 2022 08:08:13 GMT+0000 (Coordinated Universal Time)
+$(function(){var e=$('<div class="code_lang" title="代码语言"></div>');$("pre").before(e);$("pre").each(function(){var e=$(this).attr("class");if(!e){return true}var r=e.replace("line-numbers","").trim().replace("language-","").trim();$(this).siblings(".code_lang").text(r)})});
+//rebuild by neat 
